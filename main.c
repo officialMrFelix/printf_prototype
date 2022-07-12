8bb715086101852c1b0c915aba7319e4067db73e
officialMrFelix <felixobianozie@hotmail.com>
2022-07-12 21:12:43 +0000

    Included test file: main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - test file
+ * Program starts execution from here
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int count_std = 0;
+	int count_hmd = 0;
+
+	write(1,"STANDARD:\n",10);
+	count_std = printf("My name starts with: %c\n", 'F');
+	printf("I printed %d characters\n", count_std);
+
+	write(1,"________________________\n",25);
+
+	write(1,"HOME_MADE:\n",11);
+	count_hmd = _printf("My name starts with: %c\n", 'F');
+	printf("I printed %d characters\n", count_hmd);
+
+	return (0);
+}
