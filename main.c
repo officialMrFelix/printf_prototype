4a9a98a5c53eabebe57111800f4d93e3c44838cb
officialMrFelix <felixobianozie@hotmail.com>
2022-07-12 21:49:03 +0000

    Handling of %s is implemented

--- main.c
+++ main.c
@@ -15,13 +15,13 @@ int main(void)
 	int count_hmd = 0;
 
 	write(1,"STANDARD:\n",10);
-	count_std = printf("My name starts with: %c\n", 'F');
+	count_std = printf("My name is %s \n", "Felix");
 	printf("I printed %d characters\n", count_std);
 
 	write(1,"________________________\n",25);
 
 	write(1,"HOME_MADE:\n",11);
-	count_hmd = _printf("My name starts with: %c\n", 'F');
+	count_hmd = _printf("My name is %s\n", "Felix");
 	printf("I printed %d characters\n", count_hmd);
 
 	return (0);
--- print_str.c
+++ print_str.c
@@ -4,11 +4,25 @@
  * print_str - prints a variadic string each per time to stdout
  * @args: variadic parameter
  *
- * Return: number of characters printed
+ * Return: number of characters printed on Success
+ * upon failure -1
  */
 
 int print_str(va_list args)
 {
-	//expressions;
-	return (0);
+	int i;
+	int count = 0;
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		return (-1);
+
+	while (str[i])
+	{
+		count = write(1, &str[i], 1);
+		i = i + 1;
+	}
+
+	return (count);
 }	
